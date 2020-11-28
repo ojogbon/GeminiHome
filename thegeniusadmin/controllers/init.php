cfd0d60802b288c5a00363083711f45c3654b449
ojogbon <40061054+ojogbon@users.noreply.github.com>
2020-11-28 11:35:02 -0800

    edit init  to test

--- thegeniusadmin/controllers/init.php
+++ thegeniusadmin/controllers/init.php
@@ -10,5 +10,5 @@ $companyname = "AremoGemini";
 $parent_path = $dev_path == "dev" ? "/GeminiHome/thegeniusadmin/" : "/app/thegeniusadmin/";
 
 
-    include ($_SERVER['DOCUMENT_ROOT']. $parent_path.'models/MainModel.php');
-    $mainModel = new MainModel();
+    // include ($_SERVER['DOCUMENT_ROOT']. $parent_path.'models/MainModel.php');
+    // $mainModel = new MainModel();
