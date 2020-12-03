59b3bae631d9878517c5f7e91129807304e2e96b
ojogbon <40061054+ojogbon@users.noreply.github.com>
2020-12-02 22:58:36 -0800

    edit  init, display universal path  for test  code on heroku

--- thegeniusadmin/controllers/init.php
+++ thegeniusadmin/controllers/init.php
@@ -4,13 +4,13 @@ use http\Client;
 
 session_start();
 
-$dev_path = "dev";
+$dev_path = "pro";
 $companyname = "AremoGemini";
 
 $parent_path = $dev_path == "dev" ? "GeminiHome/thegeniusadmin/" : "thegeniusadmin/";
 
 
 echo $parent_path;
-    // include ($_SERVER['DOCUMENT_ROOT']."/". $parent_path.'models/MainModel.php');
-    // echo $_SERVER['DOCUMENT_ROOT']."/". $parent_path.'models/MainModel.php';
+     //include ($_SERVER['DOCUMENT_ROOT']."/". $parent_path.'models/MainModel.php');
+     echo $_SERVER['DOCUMENT_ROOT']."/". $parent_path.'models/MainModel.php';
     // $mainModel = new MainModel();
