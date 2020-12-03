e0dca45787c12a35944b872d85faab78cc405b24
ojogbon <40061054+ojogbon@users.noreply.github.com>
2020-12-02 23:02:10 -0800

    edit  init uncomment  display path  for test  code on heroku

--- thegeniusadmin/controllers/init.php
+++ thegeniusadmin/controllers/init.php
@@ -10,7 +10,6 @@ $companyname = "AremoGemini";
 $parent_path = $dev_path == "dev" ? "GeminiHome/thegeniusadmin/" : "thegeniusadmin/";
 
 
-echo $parent_path;
-     //include ($_SERVER['DOCUMENT_ROOT']."/". $parent_path.'models/MainModel.php');
+     include ($_SERVER['DOCUMENT_ROOT']."/". $parent_path.'models/MainModel.php');
      echo $_SERVER['DOCUMENT_ROOT']."/". $parent_path.'models/MainModel.php';
     // $mainModel = new MainModel();
